cabf08f081a59bba87ae4d5dd901745909817926
Paulo Queiroz <pvaqueiroz@gmail.com>
2020-01-05 06:09:58 -0800

    Better test.sh, updated runner

--- test.sh
+++ test.sh
@@ -4,13 +4,13 @@ MODULE="proton"
 MANIFEST="com.raggesilver.Proton.json"
 
 if [ ! -d "app" ]; then
-  flatpak-builder --stop-at=$MODULE app $MANIFEST
+  flatpak-builder --stop-at=$MODULE app $MANIFEST || exit $?
 fi
 
 if [ ! -d "app_build" ]; then
-  flatpak-builder --run app $MANIFEST meson --prefix=/app app_build
+  flatpak-builder --run app $MANIFEST meson --prefix=/app app_build || exit $?
 fi
 
-flatpak-builder --run app $MANIFEST ninja -C app_build
-flatpak-builder --run app $MANIFEST ninja -C app_build install
-flatpak-builder --run app $MANIFEST $MODULE
+flatpak-builder --run app $MANIFEST ninja -C app_build || exit $?
+flatpak-builder --run app $MANIFEST ninja -C app_build install || exit $?
+flatpak-builder --run app $MANIFEST $MODULE || exit $?
